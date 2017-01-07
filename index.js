f8b5b71fba0356290f7cfe7cc279c0c236ea4fce
Ersel Aker <erselaker@gmail.com>
2017-01-07 03:08:56 +0000

    WIP

---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const program = require('commander');
+const osascript = require('node-osascript');
+const spotify = require('spotify-web-api-node');
+const _ = require('lodash');
+const searchResultParser = require('./parsers/');
+
+const spotifyApi = new spotify();
+
+const SearchOptions = {
+	'track': {
+		'fn': spotifyApi.searchTracks.bind(spotifyApi),
+		'type': 'tracks'
+	},
+	't': {
+		'fn': spotifyApi.searchTracks.bind(spotifyApi),
+		'type': 'tracks'
+	},
+	'artist': {
+		'fn': spotifyApi.searchArtists.bind(spotifyApi),
+		'type': 'artists'
+	},
+	'ar': {
+		'fn': spotifyApi.searchArtists.bind(spotifyApi),
+		'type': 'artists'
+	},
+	'album': {
+		'fn': spotifyApi.searchAlbums.bind(spotifyApi),
+		'type': 'albums'
+	},
+	'al': {
+		'fn': spotifyApi.searchAlbums.bind(spotifyApi),
+		'type': 'albums'
+	},
+	'playlist': {
+		'fn': spotifyApi.searchPlaylists.bind(spotifyApi),
+		'type': 'playlists'
+	},
+	'p': {
+		'fn': spotifyApi.searchPlaylists.bind(spotifyApi),
+		'type': 'playlists'
+	}
+};
+
+program
+	.version('0.0.1')
+	.command('search <type> [query...]')
+	.action((type, query) => {
+		// search for tracks by default
+		var searchFn = SearchOptions['track'].fn;
+		var searchQry = `${type} ${query.join(' ')}`;
+		var resultType = SearchOptions['track'].type;
+
+		if (SearchOptions[type]){
+			searchFn = SearchOptions[type].fn;
+			searchQry = query.join(' ');
+			resultType = SearchOptions[type].type;
+		}
+
+		searchFn(searchQry)
+			.then(function(data) {
+				console.log(searchResultParser(resultType, data));
+			}, function(err) {
+				console.error(err);
+			});
+	});
+
+program.parse(process.argv);
---
+++ parsers/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const moment = require('moment');
+// custom moment plugin for formatting track duractions
+require("moment-duration-format");
+
+const SearchResultParsers = {
+	'albums': albumParserFn,
+	'artists': artistParserFn,
+	'playlists': playlistParserFn,
+	'tracks': trackParserFn,
+}
+
+function albumParserFn(resultType, data){
+	throw new Error('not implemented');
+}
+
+function artistParserFn(resultType, data){
+	throw new Error('not implemented');
+}
+
+function playlistParserFn(resultType, data){
+	throw new Error('not implemented');
+}
+
+function trackParserFn(resultType, data){
+	return data.body[resultType].items.map((resultEntry, index) => {
+		var resultIndex = index + 1;
+		var trackName = resultEntry.name;
+		var albumName = resultEntry.album.name;
+		var artistNames = resultEntry.artists.map((artist) => artist.name ).join(", ");
+		var duration = moment.duration(resultEntry.duration_ms, 'milliseconds').format();
+
+		return {
+			resultIndex,
+			trackName,
+			albumName,
+			artistNames,
+			duration
+		};
+	});
+}
+
+function parseSearchResults(resultType, data){
+	if(SearchResultParsers[resultType]){
+		return SearchResultParsers[resultType](resultType, data);
+	} else {
+		throw new Error(`${resultType} can not be parsed.`);
+	}
+}
+
+
+module.exports = parseSearchResults;
